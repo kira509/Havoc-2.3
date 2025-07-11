9167a03c8f551641d6af890ddf29578b23566820
kira509 <tacetygre87@gmail.com>
2025-07-11 12:22:57 +0300

    Update hisoka.js

--- src/hisoka.js
+++ src/hisoka.js
@@ -1,9 +1,9 @@
 import config from "../config.js"
 import { LocalAuth } from "whatsapp-web.js"
-import puppeteer from "puppeteer"
 import chokidar from "chokidar"
-import { platform } from "os"
+import puppeteer from "puppeteer"
 import path from "path"
+import { platform } from "os"
 
 import API from "./lib/lib.api.js"
 import Function from "./lib/lib.function.js"
@@ -27,25 +27,62 @@ async function start() {
     : content || {}
   await database.write(global.db)
 
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
+  })
+
   const hisoka = new Client({
     authStrategy: new LocalAuth({
       dataPath: `./${config.session.Path}`,
       clientId: `${config.session.Name}`
     }),
-    puppeteer: {
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu'
-      ]
-    },
-    takeoverTimeoutMs: 'Infinity',
-    autoClearSession: true
+    puppeteer: browser,
+    takeoverTimeoutMs: 0,
+    authTimeoutMs: 0,
+    qrMaxRetries: 0
   })
 
+  // ✅ Use Pairing Code Instead of QR
   hisoka.once("require_pairing_code", async () => {
-    const code = await hisoka.requestPairingCode("254738701209") // your number without +
-    console.log(`🔐 Pairing Code
+    const code = await hisoka.requestPairingCode(config.options.owner[0])
+    console.log(`🔐 Pairing Code: ${code}`)
+  })
+
+  hisoka.on("ready", () => console.info("✅ GenesisBot connected!"))
+  hisoka.on("auth_failure", console.error)
+  hisoka.on("disconnected", () => start())
+
+  hisoka.on("message_create", async (msg) => {
+    const m = await serialize(hisoka, msg)
+    await Message(hisoka, m)
+  })
+
+  // autosave DB
+  setInterval(async () => {
+    if (global.db) await database.write(global.db)
+  }, 30000)
+
+  hisoka.initialize()
+}
+
+start()
+
+let choki = chokidar.watch(Func.__filename(path.join(process.cwd(), 'src', 'commands')), { ignored: /^\./ })
+choki
+  .on('change', async Path => {
+    const command = await import(Func.__filename(Path) + "?v=" + Date.now())
+    global.commands.set(command?.default?.name, command)
+  })
+  .on('add', async Path => {
+    const command = await import(Func.__filename(Path) + "?v=" + Date.now())
+    global.commands.set(command?.default?.name, command)
+  })
